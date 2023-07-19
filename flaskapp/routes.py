ba228dbae8df5185564e35d841e6669c90c467ea
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-07-19 16:10:34 +0330

    Initializing the first menu and bot_fother first setup.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -6,8 +6,26 @@ from flaskapp import app, bot_methods
 def index():
     if request.method == 'POST':
         msg = request.get_json()
-        print(f"<h1>{msg}</h1>")
-        bot_methods.send_message(msg, "112042461")
+        # chat_id = msg['message']['chat']['id']
+        if text_check(msg)
+        greeting(msg)
         return Response('ok', status=200)
     else:
         return '<h1>Asazoon Telegram Bot</h1>'
+
+
+def text_check(msg):
+    try:
+        is_text = msg['message']['text']
+    except KeyError as error:
+        print("Text not found", error)
+        is_text = None
+    return is_text
+
+
+def greeting(msg):
+    text = msg['message']['text']
+    chat_id = msg['message']['chat']['id']
+    greet = "در خدمت شما هستم برای شروع از منوی زیر استفاده نمایید."
+    if text is "/start":
+        bot_methods.send_message(msg, chat_id)
