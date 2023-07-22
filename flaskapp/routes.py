94ac2fb89b3931377863e760f686136f1e2918f6
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-07-22 17:20:30 +0330

    Add forward method to send new registered user to a channel.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -145,7 +145,7 @@ def check_name(msg):
             info += "\n"
         bot_methods.send_message(
             f"شما با اطلاعات زیر در آسازون ثبت نام شدید:\n{info}", chat_id)
-        bot_methods.forward_message("ok", -1001971910516, 5250278182)
+        bot_methods.send_message(f"{info}", -1001971910516)
 
 
 def update_info(file_path, line_number, new_data):
